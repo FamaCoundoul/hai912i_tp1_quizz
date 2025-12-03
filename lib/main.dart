917ee7eae9fd63a8eec266f1fbcf805d3948bcdf
njap <njap@njaps-MacBook-Pro.local>
2025-12-03 09:39:16 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'question_model.dart';
+import 'quizz_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final List<Question> questions = await _loadQuestionsFromJson();
+  runApp(MyApp(questions: questions));
+}
+
+Future<List<Question>> _loadQuestionsFromJson() async {
+  final String jsonData =
+  await rootBundle.loadString('assets/data/quizz_questions.json');
+  final List<dynamic> list = json.decode(jsonData);
+  return list.map((e) => Question.fromJson(e)).toList();
+}
+
+class MyApp extends StatelessWidget {
+  final List<Question> questions;
+
+  const MyApp({Key? key, required this.questions}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Quizz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        useMaterial3: true,
+      ),
+      home: QuizzPage(title: "Questions/Réponses", questions: questions),
+    );
+  }
+}
---
+++ lib/question_model.dart
@@ -0,0 +1,33 @@
+class Question {
+  final String id;
+  final String questionText;
+  final bool isCorrect;
+  final String theme;
+  final int difficulty;
+  final String image;
+
+  Question({
+    required this.id,
+    required this.questionText,
+    required this.isCorrect,
+    required this.theme,
+    required this.difficulty,
+    required this.image
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      id: json['id'],
+      questionText: json['questionText'],
+      isCorrect: json['isCorrect'],
+      theme: json['theme'] ?? "",
+      difficulty: json['difficulty'] ?? 1,
+      image: json['image'] != null
+          ? json['image']!.replaceFirst('./', 'assets/')
+          : "assets/images/placeholder.png",
+
+
+
+    );
+  }
+}
